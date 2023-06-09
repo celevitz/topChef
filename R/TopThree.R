76a984dbf24662189d61cf244d2ed1ef0ab300bf
Carly <celevitz@gmail.com>
2023-06-09 09:36:43 -0700

    Add cumulative total viz

--- R/TopThree.R
+++ R/TopThree.R
@@ -155,6 +155,123 @@ allseasons <- allseasons %>%
         arrange(series,sznnumber,episode,chef) %>%
         mutate(indexcumulative = cumsum(index))
 
+    # keep just top three
+      cumulativeT3 <- cumulative %>%
+        filter((placement <=3 | is.na(placement)) & series == "US") %>%
+        mutate(placementstring = case_when(placement == 1 ~ "1st"
+                                           ,placement == 2 ~ "2nd"
+                                           ,placement == 3 ~ "3rd"
+                                           ,is.na(placement) ~ "Current season"
+                                           ,TRUE ~ "")) %>%
+        # what is the max episode by chef? use this to create labels
+        group_by(sznnumber,chef) %>%
+        mutate(maxepi = max(episode)
+               ,cheflabelall = ifelse(episode == maxepi,paste0(str_split_fixed(chef," ",2)[,1]," (S",sznnumber,")"),NA)
+               # make some of the labels be not at the end
+               ,cheflabelall = case_when(chef == "Stefan R." & sznnumber == 5 & episode == 11 ~ "Stefan (S5)"
+                                         ,chef == "Stefan R." & sznnumber == 5 & episode == 14 ~ NA
+                                         ,chef == "Gregory G." & sznnumber == 12 & episode == 5 ~ "Gregory (S12)"
+                                         ,chef == "Gregory G." & sznnumber == 12 & episode == 15 ~ NA
+                                         ,chef == "Doug A." & sznnumber == 12 & episode == 9 ~ "Doug (S12)"
+                                         ,chef == "Doug A." & sznnumber == 12 & episode == 14 ~ NA
+                                         ,chef == "Kevin G." & sznnumber == 6 & episode == 14 ~ "Kevin (S6)"
+                                         ,chef == "Kevin G." & sznnumber == 6 & episode == 15 ~ NA
+                                         ,chef == "Tiffani F." & placement == 2 ~ NA
+                                         ,chef == "Carla H." & placement == 2 ~ NA
+                                         ,chef == "Amar S." & placement == 2 ~ NA
+                                         ,chef == "Shirley C." & placement == 2 ~ NA
+                                         ,chef == "Angelo S." & placement == 2 ~ NA
+                                         ,chef == "Evelyn G." & placement == 2 ~ NA
+                                         ,chef == "Ed C." & placement == 2 ~ NA
+                                         ,chef == "Dawn B." & placement == 2 ~ NA
+                                         ,chef == "Shota N." & placement == 2 ~ NA
+                                         ,chef == "Marcel V." & placement == 2 ~ NA
+                                         ,chef == "Casey T." & placement == 2 ~ NA
+                                         ,chef == "Sam T." & placement == 3 ~ NA
+                                         ,chef == "Sheldon S." & placement == 3 & sznnumber == 14~ NA
+                                         ,chef == "Joe S." & placement == 3 & sznnumber == 15~ NA
+                                         ,chef == "Harold D." & placement == 1 ~ NA
+                                         ,chef == "Ilan H." & placement == 1 ~ NA
+                                         ,chef == "Gabe E." & placement == 1 ~ NA
+                                         ,TRUE ~ cheflabelall)
+               ,alignment = case_when(chef == "Stephanie I." & placement == 1 ~ 1
+                                      ,chef == "Hung H." & placement == 1 ~ .5
+                                      ,chef == "Richard B." & placement == 1 ~ 1
+                                      ,chef == "Gregory G." & placement == 2 ~ 1
+                                      ,chef == "Stefan R." & placement == 2 ~ 1
+                                      ,chef == "Doug A." & placement == 3 ~ 1
+                                      ,TRUE ~ 0)
+               ,xposition = case_when(alignment == 0 ~ episode+.05
+                                      ,alignment == 1 ~ episode - .05
+                                      ,TRUE ~ episode))
+
+  ## Viz
+    # set it up
+      bkg_col <- "lightcyan"
+      title_col <- subtitle_col <- caption_col <- text_col <- "darkslategray"
+
+      titletext <- "Top Chef cumulative weighted index score by episode"
+      captionsource <- str_glue("Visualization: @carlylevitz &bull; Source: github.com/celevitz/topChef &bull; Tools: #rstats #ggplot #ggtext #tidyverse")
+      scoring <- str_glue("Methodology for weighted index scores: Elimination win = 7 points &bull; Elimination high = 3 points<br>Elimination low = - 3 points &bull; Eliminated = - 7 points &bull; Quickfire win = 4 points <br> Quickfire high = 2 points &bull; Quickfire low = - 2 points<br><br>")
+      captiontext <- str_glue("{scoring} {captionsource}")
+      subtitletext <- "Top three chefs across all seasons - *Do not spoil Season 20 Finale!*"
+
+    # plot it
+      cumulativeT3 %>%
+        arrange(sznnumber,episode,chef) %>%
+        ggplot(aes(x=episode,y=indexcumulative,color=chef,label=cheflabelall)) +
+          geom_hline(yintercept=0, color="#ffbc69")  +
+          labs(title=titletext
+               ,subtitle=subtitletext
+               ,caption= captiontext) +
+          ylab("Cumulative Weighted Index Score") + xlab("Episode") +
+          facet_wrap(~placementstring) +
+          geom_point(aes(x=episode,y=indexcumulative,color=chef)) +
+          geom_line(aes(x=episode,y=indexcumulative,color=chef)) +
+          geom_text(aes(x=xposition,y=indexcumulative),col=text_col,hjust=cumulativeT3$alignment) +
+          scale_y_continuous(lim=c(-20,70),breaks=seq(-20,70,10),labels=seq(-20,70,10)) +
+          scale_x_continuous(lim=c(1,20),breaks=seq(1,16,2),labels=seq(1,16,2)) +
+          theme(legend.position    = "none"
+                ,panel.grid        = element_blank()
+                ,panel.background  = element_rect(fill=bkg_col,color=bkg_col)
+
+                ,plot.margin       = margin(t=10,r=10,b=10,l=10)
+                ,plot.background   = element_rect(fill=bkg_col,color=bkg_col)
+                ,plot.title        = element_markdown(
+                  size    = 24
+                  ,face   = "bold"
+                  ,color  = title_col
+                  ,margin = margin(t=10,b=5))
+                ,plot.subtitle     = element_markdown(
+                  size    = 20
+                  ,color  = subtitle_col
+                  ,margin = margin(t=5,b=10))
+                ,plot.caption      = element_markdown(
+                  size    = 15
+                  ,color  = caption_col
+                  ,hjust  = .5
+                  ,halign = .5)
+
+                ,strip.text        = element_textbox(size     = 15
+                                                     ,face     = 'bold'
+                                                     ,fill    = text_col
+                                                     ,color = "white")
+                ,strip.background = element_rect(fill=text_col)
+
+                ,axis.ticks.y      = element_line(color=text_col)
+                ,axis.line.y       = element_line(color=text_col)
+                ,axis.line.x       = element_line(color=text_col)
+                ,axis.ticks.x      = element_line(color=text_col)
+
+                ,axis.text=element_text(size=18,color=text_col)
+                ,axis.title.x = element_text(size     = 18
+                                             ,color   = text_col)
+                ,axis.title.y = element_text(size     = 18
+                                             ,color   = text_col
+                                             ,margin  = margin(l=15,r=15) ) )
+
+      dev.print(png, file = paste(savedirectory,"TopThreeIndexScores_Cumulative.png",sep=""), width =1100, height = 900)
+      dev.off()
 
 
 
