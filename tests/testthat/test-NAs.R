658c01d65dd62f27c7a6d04a03306405a89978c5
Carly <celevitz@gmail.com>
2023-06-19 11:41:20 -0700

    Update tests with new variable names

--- tests/testthat/test-NAs.R
+++ tests/testthat/test-NAs.R
@@ -6,22 +6,22 @@ library(topChef)         # lout my package
   ## A. Chef Details
     # NAs are okay in: hometown, city, state, age, poc, occupation,
     # and placement in season 20
-      for (varname in c("name","chef","szn","sznnumber","series","gender")) {
+      for (varname in c("name","chef","season","seasonNumber","series","gender")) {
 
         test_that(paste("There are no NAs in",varname,sep=" "), {
           expect_equal(all(!(is.na(chefdetails[,varname]))),TRUE)
         })
 
       }
-      test_that("Everyone has a placement except in Season 20",
-                {expect_equal(all(!(is.na(chefdetails$placement[chefdetails$szn
-                                              != "World All Stars"]))),TRUE)})
+      test_that("Everyone has a placement",
+                {expect_equal(all(!(is.na(chefdetails$placement))),TRUE)})
+
   ## B. Challenge descriptions
       # NAs are okay in challenge description (this will change over time as
       # I fill them in),shop time, shop budget, prep time, cook time, product
       # placement, advantage, LCK stuff & RW stuff
-      for (varname in c("szn","sznnumber","series","episode","challenge_type"
-                        ,"outcome_type")) {
+      for (varname in c("season","seasonNumber","series","episode"
+                        ,"challengeType","outcomeType")) {
 
         test_that(paste("There are no NAs in",varname,sep=" "), {
           expect_equal(all(!(is.na(challengedescriptions[,varname]))),TRUE)
@@ -29,8 +29,8 @@ library(topChef)         # lout my package
       }
   ## C. Challenge wins
       # NAs are okay in rating
-      for (varname in c("szn","sznnumber","series","episode","in.competition"
-                        ,"chef","challenge_type")) {
+      for (varname in c("season","seasonNumber","series","episode",
+                        "inCompetition","chef","challengeType")) {
 
         test_that(paste("There are no NAs in",varname,sep=" "), {
           expect_equal(all(!(is.na(challengewins[,varname]))),TRUE)
@@ -40,13 +40,13 @@ library(topChef)         # lout my package
       test_that("The NAs in Outcome are only if they aren't in the competition"
                 , {
         expect_equal(all(!(is.na(challengewins$outcome[challengewins$
-                                             in.competition == "TRUE"]))),TRUE)
+                                             inCompetition == "TRUE"]))),TRUE)
       })
   ## D. Episode info
         # NAs are okay in overall episode number, episode name, air date as I
         # work towards complete data
-        for (varname in c("szn","sznnumber","series","episode"
-                          ,"#.of.competitors")) {
+        for (varname in c("season","seasonNumber","series","episode"
+                          ,"nCompetitors")) {
 
           test_that(paste("There are no NAs in",varname,sep=" "), {
             expect_equal(all(!(is.na(episodeinfo[,varname]))),TRUE)
@@ -54,8 +54,8 @@ library(topChef)         # lout my package
         }
   ## E. Judges
         ## NAs are okay for the competed on TC and other shows variable
-        for (varname in c("szn","sznnumber","series","episode","challenge_type"
-                          ,"outcome_type","guestjudge")) {
+        for (varname in c("season","seasonNumber","series","episode"
+                          ,"challengeType","outcomeType","guestJudge")) {
 
           test_that(paste("There are no NAs in",varname,sep=" "), {
             expect_equal(all(!(is.na(judges[,varname]))),TRUE)
@@ -63,8 +63,9 @@ library(topChef)         # lout my package
         }
   ## F. Rewards
         ## NAs are not okay
-        for (varname in c("szn","sznnumber","series","episode","challenge_type"
-                          ,"outcome_type","reward_type","reward","chef")) {
+        for (varname in c("season","seasonNumber","series","episode"
+                          ,"challengeType","outcomeType","rewardType","reward"
+                          ,"chef")) {
 
           test_that(paste("There are no NAs in",varname,sep=" "), {
             expect_equal(all(!(is.na(rewards[,varname]))),TRUE)
@@ -74,7 +75,7 @@ library(topChef)         # lout my package
 ## 2. Test Index Calculation
   ## A. Does it output a data frame?
         test_that("index calculation returns a data frame", {
-          indexoutput <- weightedindex(seriesname="US",seasonnumber=20
+          indexoutput <- weightedindex(seriesname="US",seasonnumberofchoice=20
                                        ,numberofelimchalls=10
                                        ,numberofquickfires=8)
           expect_s3_class(indexoutput,"data.frame")
