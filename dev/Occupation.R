21ccbbd5f6932face4502af513a28c44e7ddde56
Carly <celevitz@gmail.com>
2024-03-25 21:32:45 -0700

    Move occupation analysis

--- dev/Occupation.R
+++ dev/Occupation.R
@@ -3,12 +3,22 @@ library(tidyverse)
 library(devtools)
 install_github("celevitz/topChef")
 
-chefs <- data.frame(topChef::chefdetails)
+chefs <- topChef::chefdetails
 
 chefs %>%
   filter(series == "US") %>%
   group_by(occupation_category,series) %>%
-  summarise(cnumberofchefs=n())
+  summarise(numberofchefs=n())
 
 
+chefdetails %>%
+  filter(series == "US") %>%
+  group_by(occupation_category) %>%
+  summarise(n=n())
 
+chefdetails %>%
+  filter(series == "US" & !(is.na(occupation))) %>%
+  group_by(occupation_category) %>%
+  summarise(n=n()) %>%
+  mutate(percent=n/sum(n)) %>%
+  arrange(desc(percent))
