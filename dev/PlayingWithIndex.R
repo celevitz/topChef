dbf9e198d3eaedafd67a989b4ce364814f43087e
Carly <celevitz@gmail.com>
2024-03-28 22:44:25 -0700

    season 21 ep 2 analysis

--- dev/PlayingWithIndex.R
+++ dev/PlayingWithIndex.R
@@ -49,13 +49,34 @@ for (season in seq(2,21,1)) {
 
 }
 
+ep1scores <- chefdetails %>%
+  filter(seasonNumber == 21 & series == "US") %>%
+  select(chef) %>%
+  mutate(indexWeight1=case_when(chef == "Manny Barella" ~ 7
+                               ,chef %in% c("Michelle Wallace","Danny Garcia") ~ 3
+                               ,chef %in% c("Amanda Turner","Kenny Nguyen") ~ -3
+                               ,chef == "David Murphy" ~ -7
+                               ,TRUE ~ 0))
+
 temp %>%
   filter(seasonNumber == 21) %>%
-  arrange(desc(indexWeight)) %>%
-  select(chef,indexWeight)
+  select(chef,indexWeight) %>%
+  full_join(ep1scores) %>%
+  mutate(indexWeight=ifelse(is.na(indexWeight),0,indexWeight)) %>%
+  rename(indexWeight2=indexWeight) %>%
+  arrange(desc(indexWeight1))  %>%
+  mutate(rank1=rank(-indexWeight1,ties.method = "min")) %>%
+  arrange(desc(indexWeight2)) %>%
+  mutate(rank2=rank(-indexWeight2,ties.method = "min")) %>%
+  select(chef,rank1,rank2) %>%
+  mutate(diff=-(rank2-rank1))
+
 
 temp %>%
   filter(placement == 1) %>%
   arrange(desc(indexWeight)) %>%
-  select(chef,seasonNumber,indexWeight)
+  select(chef,seasonNumber,indexWeight) %>%
+  full_join(chefdetails %>%
+              filter(placement == 1 & series == "US") %>%
+              select(chef,season,seasonNumber,series,placement))
 
