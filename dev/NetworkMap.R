42a613cb1096602d25147e1d1ca9691a3b70a9a6
Carly <celevitz@gmail.com>
2024-04-17 13:42:15 -0700

    Top chef network map

--- dev/NetworkMap.R
+++ dev/NetworkMap.R
@@ -1,8 +1,18 @@
+# Carly Levitz
+# 2024-04-17
+
+########################################################################################
+## 1. Set up (packages, data)
 rm(list=ls())
 library(tidyverse)
 library(devtools)
 library(igraph)
 library(ggpubr)
+library(glue)
+library(ggtext)
+library(showtext)
+library(ggraph)
+library(tidygraph)
 
 devtools::install_github("celevitz/topChef")
 devtools::install_github("celevitz/touRnamentofchampions")
@@ -19,10 +29,11 @@ TOCchefs <- touRnamentofchampions::results %>%
 
 
 allchefs <- tcChefs %>% rbind(TOCchefs) %>%
-  filter(series %in% c("US") &
-           season != "Wisconsin")
+  # for now, I just want to look at the top chefs
+  filter(series %in% c("US") )
 
-
+########################################################################################
+## 2. Create the dataset that will feed into the node & vertex data
   combined <- tcChefs[1,]
   combined$name <- NULL
   combined$source <- combined$target <- NA
@@ -51,19 +62,42 @@ allchefs <- tcChefs %>% rbind(TOCchefs) %>%
   # drop the first row which is empty, plus when target == source
   combined <- combined %>% filter(!(is.na(source)) & target != source)
 
+  ## do a Top Chef only version of the data, sort of like a circle pack format
+    cp <- topChef::chefdetails %>%
+      select(series,seasonNumber,name) %>%
+      filter(series == "US") %>%
+      rename(source=seasonNumber,target=name) %>%
+      mutate(type = "seasonsource"
+             ,importance=1.1) %>%
+      rbind(topChef::chefdetails %>%
+              select(series,seasonNumber,name) %>%
+              filter(series == "US")  %>%
+              rename(target=seasonNumber,source=name)  %>%
+              mutate(type = "namesource") %>%
+              full_join(
+                # Get "importance" (i.e., number of times someone is in the data)
+                topChef::chefdetails %>%
+                  select(series,seasonNumber,name) %>%
+                  filter(series == "US")   %>%
+                  rename(source=name) %>%
+                  group_by(source) %>%
+                  summarise(importance = n())
+              ) ) %>%
+      select(!series)
 
-############################################
-## network graphs
+########################################################################################
+## 3. network graphs
   ## set up data for network graphs
   # (just for connections in here more than 1x, if you're doing TOC)
-  links <- combined %>%
+  # edges
+  edges <- combined %>%
     group_by(source,target) %>%
-    summarise(importance = n()) %>%
+    summarise(importance = n())
     # %>%
     #   filter(importance >1)
 
     # shorten the names
-    # RIGHT NOW THIS ISN"T WORKING -- check out links %>% view()
+    # RIGHT NOW THIS ISN"T WORKING -- check out edges %>% view()
     # mutate(tempsource = paste0(str_split(source," ")[[1]][1]," "
     #                            ,substr(str_split(source," ")[[1]][2],1,1))
     #        ,temptarget = paste0(str_split(target," ")[[1]][1]," "
@@ -72,45 +106,59 @@ allchefs <- tcChefs %>% rbind(TOCchefs) %>%
     # select(!c(source,target)) %>%
     # rename(source=tempsource,target=temptarget)
 
-
-  nodes <- data.frame(links %>% ungroup() %>%
+  # vertices
+  vertices <- data.frame(edges %>% ungroup() %>%
                         group_by(source) %>%
                         summarise(n=n()) %>%
                         select(!n) %>%
                         rename(name= source))
 
-  network <- graph_from_data_frame(d=links
-                                   , vertices=nodes
+  # graph object
+  graph <- graph_from_data_frame(d=edges
+                                   , vertices=vertices
                                    , directed=F)
+  # Calculate node importance, e.g., based on degree
+  V(graph)$importance <- degree(graph, mode = 'in')
 
-  # V(network)$color <- ifelse(V(network)$colorid == 1, "white", "white")
-  # V(network)$frame.color <- ifelse(V(network)$colorid == 1, "white", "white")
-  # V(network)$shape <- ifelse(V(network)$colorid == 1, "rectangle", "circle")
-  # V(network)$size <- ifelse(V(network)$colorid == 1, 10, 3)
-  # V(network)$size2 <- ifelse(V(network)$colorid == 1, 3, 3)
-  # V(network)$label.cex <- ifelse(V(network)$colorid == 1, 1.3, .7)
-  # V(network)$label.cex <- ifelse(V(network)$colorid == 1, 1, .8)
-  # V(network)$label.color <- ifelse(V(network)$colorid == 1, "navyblue", "black")
+  threshold <- 40
 
-  V(network)$label.cex <- .5
-  V(network)$size <- 3
-  plot(network
-       ,edge.width = E(network)$importance*.1
-       ,vertex.color="white"
-       ,layout=layout.fruchterman.reingold)
+  # circlepack edges & vertices
+  edges <- cp %>% filter(type == "namesource")
+  vertices <- cp %>%
+    select(source,type,importance) %>%
+    rename(name=source) %>% distinct()
 
-  #plot(network
-       #,edge.width=E(network)$importance*.4
-       #,edge.color=my_color
-       #,edge.arrow.size=.5
-       #, edge.curved=seq(-0.5, 0.5, length = ecount(network))
-       #,vertex.color="gray90"
-       #,vertex.label.cex=1.3
-       #,vertex.size=8
-       #,vertex.size2=3
-       #,main = "ORG Network"
-       #,sub="@carly.sue.bear"
-       #,layout=layout.fruchterman.reingold
-       #, main="fruchterman.reingold"
-  #)
+  cp_graph <- graph_from_data_frame(d=edges
+                                    , vertices=vertices
+                                    , directed=F)
+
+########################################################################################
+## 4. Visualization
+
+  # Aesthetics
+  V(cp_graph)$color <- case_when(V(cp_graph)$importance == 1 ~ "darkcyan"
+                                 ,V(cp_graph)$importance == 1.1 ~ "orange"
+                                 ,V(cp_graph)$importance == 2 ~ "white"
+                                 ,V(cp_graph)$importance == 3 ~ "white")
+  V(cp_graph)$frame.color <- case_when(V(cp_graph)$importance == 1 ~ "darkcyan"
+                                       ,V(cp_graph)$importance == 1.1 ~ "orange"
+                                       ,V(cp_graph)$importance == 2 ~ "navyblue"
+                                       ,V(cp_graph)$importance == 3 ~ "navyblue")
+  V(cp_graph)$label.cex <- case_when(V(cp_graph)$importance == 1.1~  .5
+                                     ,V(cp_graph)$importance > 1.1~  .4
+                                     ,TRUE ~ 0.01)
+  V(cp_graph)$size <- case_when(V(cp_graph)$importance == 1 ~ 1
+                                ,V(cp_graph)$importance == 1.1 ~ 5
+                                ,V(cp_graph)$importance == 2 ~ .5
+                                ,V(cp_graph)$importance == 3 ~ .5)
+
+  # Plot
+  plot.igraph(cp_graph
+       ,layout = layout.fruchterman.reingold
+       ,asp = 0
+       ,main="Top Chef Network across Season"
+       ,sub="IG: @carly.sue.bear /// X: @carlylevitz"
+       )
+
+
 
