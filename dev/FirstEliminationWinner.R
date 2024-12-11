b4db5407968c90f20e080f6a8aa8507ec49f4a74
Carly <celevitz@gmail.com>
2024-12-10 20:01:32 -0800

    Additional analyses about first elimination challenge winners

--- dev/FirstEliminationWinner.R
+++ dev/FirstEliminationWinner.R
@@ -1,10 +1,14 @@
 library(topChef)
 library(tidyverse)
 library(devtools)
-install_github("celevitz/topChef")
+#install_github("celevitz/topChef")
+directory <- "/Users/carlylevitz/Documents/Data/topChef/"
 
-challenges <- topChef::challengewins
-chefs <- topChef::chefdetails
+# Bring in data
+chefs <- read.csv(paste0(directory,"Top Chef - Chef details.csv"))
+challenges <- read.csv(paste0(directory,"Top Chef - Challenge wins.csv"))
+challengedescriptions <- read.csv(paste0(directory
+                                ,"Top Chef - Challenge descriptions.csv"))
 
 # number of seasons with multiple first elim winners: Charleston, NY, Texas
 challenges %>%
@@ -19,6 +23,16 @@ challenges %>%
   summarise(numberofwinners=n()) %>%
   filter(numberofwinners>1)
 
+# Seasons without an elimination challenge first episode
+challenges %>%
+  filter(series == "US" & episode == 1) %>%
+  select(season,challengeType) %>%
+  distinct() %>%
+  group_by(season) %>%
+  mutate(hadelimchallfirstep = max(case_when(challengeType %in% c("Elimination","Quickfire Elimination","Sudden Death Quickfire") ~ 1
+                   ,TRUE ~ 0))) %>%
+  filter(hadelimchallfirstep %in% 0)
+
 # Placement of chefs who won first elimination challenge
 # just those seasons with only one winner
 
@@ -31,16 +45,64 @@ placement <- challenges %>%
   group_by(season) %>%
   mutate(episodeflag = min(episode,na.rm=T)) %>%
   # keep just the winners from that episode
-  filter(outcome == "WIN" & episodeflag == episode) %>%
+  filter(outcome %in% "WIN" & episodeflag == episode) %>%
   left_join(chefs %>% select(season,seasonNumber,series,chef,placement)) %>%
-  select(season,seasonNumber,chef,challengeType,placement) %>%
+  select(season,seasonNumber,chef,placement) %>%
   distinct() %>%
-  ungroup() %>% group_by(challengeType,placement) %>%
+  ungroup() %>% group_by(placement) %>%
+  summarise(n=n()) %>%
+  mutate(foraveragesnumerator=placement*n) %>%
+  ungroup() %>%
+  mutate(averageplacement=sum(foraveragesnumerator)/sum(n))
+
+## People who won first elimination challenge and then went on to win/get 2nd
+challenges %>%
+  filter(series == "US") %>%
+  # First Elimination challenge
+  filter(challengeType %in% c("Elimination","Quickfire Elimination","Sudden Death Quickfire")) %>%
+  group_by(season) %>%
+  mutate(episodeflag = min(episode,na.rm=T)) %>%
+  # keep just the winners from that episode
+  filter(outcome %in% "WIN" & episodeflag == episode) %>%
+  left_join(chefs %>% select(season,seasonNumber,series,chef,placement)) %>%
+  select(season,seasonNumber,chef,placement) %>%
+  distinct() %>% filter(placement<4) %>% arrange(seasonNumber)
+
+## Were the elim challenges Team or Indiv?
+challenges %>%
+  filter(series == "US") %>%
+  # First Elimination challenge
+  filter(challengeType %in% c("Elimination","Quickfire Elimination","Sudden Death Quickfire")) %>%
+  group_by(season) %>%
+  mutate(episodeflag = min(episode,na.rm=T)) %>%
+  # keep just that one
+  filter(episodeflag == episode) %>%
+  select(season,seasonNumber,series,episode) %>%
+  distinct() %>%
+  left_join(challengedescriptions %>% select(season,seasonNumber,series,episode
+                                           ,challengeType,outcomeType)) %>%
+  filter(challengeType %in% c("Elimination","Quickfire Elimination","Sudden Death Quickfire")) %>%
+  group_by(challengeType,outcomeType) %>%
   summarise(n=n())
 
-sum(placement$n)
+## How did the average placement differ if they won indivudally or as a team?
+challenges %>%
+  filter(series == "US" ) %>%
+  left_join(challengedescriptions %>% select(season,seasonNumber,series,episode
+                                             ,challengeType,outcomeType)) %>%
+  # First Elimination challenge
+  filter(challengeType %in% c("Elimination","Quickfire Elimination","Sudden Death Quickfire")) %>%
+  group_by(season) %>%
+  mutate(episodeflag = min(episode,na.rm=T)) %>%
+  # keep just the winners from that episode
+  filter(outcome %in% "WIN" & episodeflag == episode) %>%
+  left_join(chefs %>% select(season,seasonNumber,series,chef,placement)) %>%
+  select(season,seasonNumber,chef,placement,outcomeType) %>%
+  distinct() %>%
+  ungroup() %>% group_by(outcomeType) %>%
+  summarise(averageplacement = mean(placement,na.rm=T))
 
-
+####################################################################
 statsbynumberofchalls <- challenges %>%
   filter(seasonNumber == 21) %>%
   group_by(chef,challengeType,outcome) %>%
