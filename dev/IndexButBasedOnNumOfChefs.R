9b0ac6dff42356a4ef4bfda3568bac46363507e6
Carly <celevitz@gmail.com>
2025-06-11 19:15:34 -0700

    Scores based on number of chefs in comp

---
+++ dev/IndexButBasedOnNumOfChefs.R
@@ -0,0 +1,88 @@
+rm(list=ls())
+library(tidyverse)
+
+directory <- "/Users/carlylevitz/Documents/Data/topChef/"
+
+numchefsofinterest <- 3
+
+challengewinsRaw <- read.csv(paste0(directory,"Top Chef - Challenge wins.csv"))%>%
+  filter(series == "US")
+
+chefdetails <- read.csv(paste0(directory,"Top Chef - Chef details.csv"))  %>%
+  filter(series == "US" )
+
+episodeinfo <- read.csv(paste0(directory,"Top Chef - Episode information.csv"))  %>%
+  filter(series == "US" )
+
+## Combine the challenge data with the # of chefs in the challenge
+## Actually, i could just use the # of chefs with In Competition == TRUE
+
+  numchefs <- challengewinsRaw %>%
+    select(series,season,seasonNumber,episode,inCompetition,chef) %>%
+    distinct() %>%
+    #mutate(inepisode = sum(ifelse(inCompetition == "TRUE",1,0))) %>%
+    group_by(series,season,seasonNumber,episode) %>%
+    summarise(numberofchefs = sum(ifelse(inCompetition == "TRUE",1,0)))
+
+  challengewins <- challengewinsRaw %>%
+    select(!c(rating,inCompetition,immune)) %>%
+    left_join(numchefs) %>%
+    # remove challenge types we're not interested in
+    filter(!(challengeType %in% c("Battle of the Sous Chefs"
+                                  ,"Qualifying Challenge"))) %>%
+    # simplify the challenge types & outcome types
+    mutate(challengeType = ifelse(challengeType %in% c("Quickfire Elimination"
+                                                   ,"Sudden Death Quickfire")
+                                  ,"Elimination",challengeType)
+           ,outcome = case_when(outcome %in% c("HIGH") ~ "HIGH"
+                               ,outcome %in% c("WIN","WINNER") ~ "WIN"
+                               ,outcome %in% c("RUNNER-UP","DISQUALIFIED"
+                                               ,"WITHDREW") ~ "OUT"
+                               ,outcome %in% c("DIDN'T COMPETE") ~ "IN"
+                               ,TRUE ~ outcome))
+
+## Scores across seasons with a given number of chefs
+  scoresDetails <- challengewins %>%
+    filter(numberofchefs > numchefsofinterest) %>%
+    select(!numberofchefs) %>%
+    group_by(series,season,seasonNumber,challengeType,outcome,chef) %>%
+    summarise(n=n()) %>%
+    # calculate the points
+    mutate(points = case_when(outcome == "HIGH" & challengeType ==
+                                                            "Elimination" ~ 3
+                      ,outcome == "WIN" & challengeType == "Elimination" ~ 7
+                      ,outcome == "LOW" & challengeType == "Elimination" ~ -3
+                      ,outcome == "OUT" & challengeType == "Elimination" ~ -7
+                      ,outcome == "HIGH" & challengeType == "Quickfire" ~ 2
+                      ,outcome == "WIN" & challengeType == "Quickfire" ~ 4
+                    ,outcome == "LOW" & challengeType == "Quickfire" ~ -2)) %>%
+    ungroup() %>%
+    left_join(chefdetails %>%
+                select(series,season,seasonNumber,chef,placement) %>%
+                mutate(placement = as.numeric(placement))) %>%
+    mutate(seasonWinner = ifelse(placement == 1, 1, 0))
+
+  # total score
+  scores <- scoresDetails%>%
+    group_by(series,season,seasonNumber,chef,placement,seasonWinner) %>%
+    summarise(points=sum(points,na.rm=T))
+
+
+## Keep just the number of chefs of interest and see if that's related to
+  ## winning
+  reg <- glm(scores$seasonWinner[scores$placement <= 3] ~
+               scores$points[scores$placement <= 3])
+  summary(reg)
+
+
+
+
+
+
+
+
+
+
+
+
+
