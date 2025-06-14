6e46f923c289b13964d702d296f89fc65bd01018
Carly <celevitz@gmail.com>
2025-06-13 17:22:13 -0700

    specific season $ by challenge type

--- dev/Money.R
+++ dev/Money.R
@@ -51,6 +51,20 @@ placement <- placement %>%
     arrange(desc(percent)) %>%
     print(n)
 
+  # by challenge type
+  s22byChallType <- rewards %>%
+    filter(rewardType %in% c( "Money","Money & prize") &
+             series == "US" & seasonNumber == 22) %>%
+    # there are a few things
+    mutate(reward = case_when(reward %in% c("$10,000 and Sous Vide machine"
+                                            ,"$10,000 and a 10 day cruise from Holland Cruise") ~ "10000"
+                              ,reward %in% "$5,000 and year of dinner from Blue Apron + your recipe featured in Blue Apron" ~ "5000"
+                              ,TRUE ~ reward)) %>%
+    group_by(season,seasonNumber,challengeType,chef) %>%
+    summarise(reward = sum(as.numeric(reward),na.rm=T)) %>%
+    pivot_wider(names_from=challengeType,values_from=reward)
+
+
 ## leaderboard
   money %>%
     filter(reward > 20000) %>%
