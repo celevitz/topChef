22a2f25d268952f9fe9637335419125568ff4097
Carly <celevitz@gmail.com>
2025-07-26 12:37:56 -0700

    RE representation at different times of the season

---
+++ dev/Representation/POCRatioAtSpecificTimes.R
@@ -0,0 +1,375 @@
+rm(list=ls())
+library(tidyverse)
+library(openxlsx)
+library(ggplot2)
+library(gt)
+
+directory <- "/Users/carlylevitz/Documents/Data/topChef/"
+chefdetails <- read.csv(paste0(directory,"Top Chef - Chef details.csv"))  %>%
+  filter(series == "US" ) %>%
+  mutate(personOfColor = ifelse(is.na(personOfColor),"white",personOfColor))
+
+## First, POC ratio at start
+starting <- chefdetails %>%
+  select(season,seasonNumber,chef,personOfColor) %>%
+  filter(!(is.na(personOfColor))) %>%
+  group_by(season,seasonNumber,personOfColor) %>%
+  summarise(n=n())%>%
+  # how many are in the comp at that time?
+  ungroup() %>%
+  group_by(season,seasonNumber) %>%
+  mutate(total = sum(n)
+         ,timing="A. All chefs, including those who didn't make it out of Qualifiers or LCK")
+
+## POC ratio of only those who were in the official competition
+officialcomp <- chefdetails %>%
+  mutate(placement=as.numeric(placement)) %>%
+  filter(!(is.na(personOfColor)) &
+           # While Season 22 is ongoing, I need to include them since their
+           # placement is NA for those still in the comp
+           (!(is.na(placement)) | seasonNumber == 22) &
+           !(chef %in% c("Ying Gao","Sam Olayinka"))
+         ) %>%
+  select(season,seasonNumber,chef,personOfColor) %>%
+  group_by(season,seasonNumber,personOfColor) %>%
+  summarise(n=n()) %>%
+  # how many are in the comp at that time?
+  ungroup() %>%
+  group_by(season,seasonNumber) %>%
+  mutate(total = sum(n)
+         ,timing="B. Chefs with an official placement in their season")
+
+## POC ratio of top 8 chefs
+eight <- chefdetails %>%
+  mutate(placement=as.numeric(placement)) %>%
+  filter(!(is.na(personOfColor)) &
+           # While Season 22 is ongoing, I need to include them since their
+           # placement is NA for those still in the comp
+           (placement <=8 |  (seasonNumber == 22 & is.na(placement)) ) &
+           !(chef %in% c("Ying Gao","Sam Olayinka"))
+        ) %>%
+  select(season,seasonNumber,chef,personOfColor) %>%
+  group_by(season,seasonNumber,personOfColor) %>%
+  summarise(n=n()) %>%
+  mutate(total=8
+         ,timing = "C. Final 8 chefs")
+
+## POC ratio of top 4 chefs
+four <- chefdetails %>%
+  mutate(placement=as.numeric(placement)) %>%
+  filter(!(is.na(personOfColor)) &
+           # While Season 22 is ongoing, I need to include them since their
+           # placement is NA for those still in the comp
+           (placement <=4 | (seasonNumber == 22 & is.na(placement)) ) &
+           !(chef %in% c("Ying Gao","Sam Olayinka"))
+  ) %>%
+  select(season,seasonNumber,chef,personOfColor) %>%
+  group_by(season,seasonNumber,personOfColor) %>%
+  summarise(n=n()) %>%
+  mutate(total=4
+         ,timing = "D. Final 4 chefs")
+
+## Bring the data together
+alldata <- starting %>%
+  bind_rows(officialcomp) %>%
+  bind_rows(eight) %>%
+  bind_rows(four) %>%
+  # Get percent of group that are women
+  mutate(percent = round(n/total,3)) %>%
+  filter(personOfColor == "POC") %>%
+  # get ratio of group that are POC
+  mutate(ratio = round(n/(total-n),3))%>%
+  select(!personOfColor) %>%
+  # which ones have more POC and which have less?
+  mutate(POC_balance = case_when(ratio < 1 ~ "A. Fewer people of color than white people"
+                             ,ratio == 1 ~ "B. Same number of people color as white people"
+                             ,ratio > 1 ~ "C. More people of color than white people"
+                             ,TRUE ~ "needs to be categorized"))
+
+##################################
+## Visualize it
+
+facetviz <- alldata %>%
+  mutate(timing = ifelse(timing %in% "A. All chefs, including those who didn't make it out of Qualifiers or LCK"
+                         ,"A. All chefs, including those who didn't\nmake it out of Qualifiers or LCK"
+                         ,timing)
+         ,timing = ifelse(timing %in% "B. Chefs with an official placement in their season"
+                          ,"B. Chefs with an official\nplacement in their season"
+                          ,timing)) %>%
+  ggplot(aes(x=seasonNumber,y=ratio
+             ,color=POC_balance,shape=POC_balance,fill=POC_balance))+
+  geom_point(size=1) +scale_x_continuous("Season #") + facet_wrap(~timing) +
+  scale_y_continuous(limits=c(0,4.6),"Ratio of people of color to white people") +
+  guides(shape=guide_legend(title="Balance",override.aes=list(size=1))
+        ,fill=guide_legend(title="Balance")
+        ,color=guide_legend(title="Balance")) +
+  ggtitle("Figure 1: Ratio of people of color to white people at different times of Top Chef seasons"
+          ,subtitle = "Values under 1 indicate fewer people of color than white people; values over 1 indicate more people of color than white people") +
+  labs(caption="Created by Carly Levitz for Pack Your Knives")+
+  scale_color_manual(values=c("#c85200","gray60","#1170AA")) +
+  scale_fill_manual(values=c("#c85200",NA,"#1170AA")) +
+  scale_shape_manual(values=c(25,16,24) ) +
+  theme(#panel.grid = element_blank()
+    panel.background = element_rect(fill="white")
+    ,plot.background = element_rect(fill="white")
+    ,plot.title.position="plot"
+    ,plot.caption = element_text(hjust=0,size=5)
+    ,plot.caption.position="plot"
+    ,strip.background =element_rect(fill="gray90")
+    ,strip.text = element_text(colour = 'black',size=8)
+    ,plot.title = element_text(size=12)
+    ,plot.subtitle = element_text(size=10)
+    ,legend.text = element_text(size=7)
+    ,legend.title = element_text(size=8)
+    ,axis.line = element_line(color="black")
+    ,axis.ticks = element_line(color="black")
+    ,axis.text = element_text(color="black"))
+ggsave(paste0(directory,"RE ratio at different times - facet scatter.png")
+       ,facetviz,width = 8,height = 4,dpi = 1200 )
+
+##################################
+# Table format
+REratiotableData <- alldata %>%
+  ungroup() %>%
+  select(season,seasonNumber,ratio,timing) %>%
+  pivot_wider(names_from=timing,values_from=ratio) %>%
+  arrange(seasonNumber) %>%
+  rename(`season #`=seasonNumber) %>%
+  ## Category of season
+  mutate(category = case_when(
+    # B < C < D = got better over time
+    # B > C > D = got worse over time
+    # B = C = D
+    `B. Chefs with an official placement in their season` > `C. Final 8 chefs`  &
+       `C. Final 8 chefs`  > `D. Final 4 chefs` ~ "Worsened over time"
+    ,`B. Chefs with an official placement in their season` < `C. Final 8 chefs`  &
+      `C. Final 8 chefs`  < `D. Final 4 chefs` ~ "Improved over time"
+    ,`B. Chefs with an official placement in their season` == `C. Final 8 chefs`  &
+      `C. Final 8 chefs`  == `D. Final 4 chefs` ~ "Stayed the same"
+    # B > C, C = D: worsened, flattened
+    ,`B. Chefs with an official placement in their season` > `C. Final 8 chefs`  &
+      `C. Final 8 chefs`  == `D. Final 4 chefs` ~ "Worsened, then stayed the same"
+    # B < C, D < B: Got better, worsened more than at start
+    ,`B. Chefs with an official placement in their season` < `C. Final 8 chefs`  &
+      `B. Chefs with an official placement in their season` > `D. Final 4 chefs` ~ "Improved, ended up worse than at start"
+    # B > C, D > B, worsened, got better than at the start
+    ,`B. Chefs with an official placement in their season` > `C. Final 8 chefs`  &
+      `B. Chefs with an official placement in their season` < `D. Final 4 chefs` ~ "Worsened, got better than at the start"
+    # B = C, C < D: same, then improved
+    ,`B. Chefs with an official placement in their season` = `C. Final 8 chefs`  &
+      `C. Final 8 chefs` < `D. Final 4 chefs` ~ "Same, then improved"
+    # B < C, C = D: improved, flattened
+    ,`B. Chefs with an official placement in their season` < `C. Final 8 chefs`  &
+      `C. Final 8 chefs` == `D. Final 4 chefs` ~ "Improved, then stayed the same"
+    ,TRUE ~ "Mixed"
+  ))
+
+REratiotable <- REratiotableData %>%
+  gt() %>%
+  cols_hide(columns=category) %>%
+  tab_source_note(source_note = "Created by Carly Levitz for Pack Your Knives") %>%
+  tab_options(data_row.padding = px(1),
+              column_labels.padding = px(1),
+              row_group.padding = px(1))  %>%
+  tab_style(style = cell_text(align = "right"),locations = cells_source_notes()) %>%
+  tab_style(style = cell_text(align = "left",weight="bold")
+            ,locations = cells_title(groups="title")) %>%
+  tab_style(style = cell_text(align = "left",weight="bold")
+            ,locations = cells_row_groups() ) %>%
+  tab_style(style = cell_text(align = "left")
+            ,locations = cells_title(groups="subtitle")) %>%
+  tab_style(style = cell_text(align = "center")
+            ,locations = cells_body(columns=!season)) %>%
+  tab_style(style = cell_text(align = "center",weight="bold")
+            ,locations = cells_column_labels(columns=!season)) %>%
+  tab_style(style = cell_text(align = "left",weight="bold")
+            ,locations = cells_column_labels(columns=season)) %>%
+  # Color the cells based on their value
+  # Category A
+    tab_style(locations=cells_body(columns=`A. All chefs, including those who didn't make it out of Qualifiers or LCK`
+                                   ,rows = `A. All chefs, including those who didn't make it out of Qualifiers or LCK` <.7)
+              ,style = cell_fill("#c85200")) %>%
+    tab_style(locations=cells_body(columns=`A. All chefs, including those who didn't make it out of Qualifiers or LCK`
+                                   ,rows = `A. All chefs, including those who didn't make it out of Qualifiers or LCK` <.7)
+              ,style = cell_text("white")) %>%
+    tab_style(locations=cells_body(columns=`A. All chefs, including those who didn't make it out of Qualifiers or LCK`
+                                   ,rows = `A. All chefs, including those who didn't make it out of Qualifiers or LCK` >=.7 &
+                                     `A. All chefs, including those who didn't make it out of Qualifiers or LCK` < 1)
+              ,style = cell_fill("#ffbc69")) %>%
+    tab_style(locations=cells_body(columns=`A. All chefs, including those who didn't make it out of Qualifiers or LCK`
+                                 ,rows = `A. All chefs, including those who didn't make it out of Qualifiers or LCK` == 1)
+              ,style = cell_fill("gray90")) %>%
+    tab_style(locations=cells_body(columns=`A. All chefs, including those who didn't make it out of Qualifiers or LCK`
+                                   ,rows = `A. All chefs, including those who didn't make it out of Qualifiers or LCK` > 1 &
+                                     `A. All chefs, including those who didn't make it out of Qualifiers or LCK` < 2)
+              ,style = cell_fill("#a3cce9")) %>%
+    tab_style(locations=cells_body(columns=`A. All chefs, including those who didn't make it out of Qualifiers or LCK`
+                                   ,rows = `A. All chefs, including those who didn't make it out of Qualifiers or LCK` >=2)
+              ,style = cell_fill("#1170AA")) %>%
+    tab_style(locations=cells_body(columns=`A. All chefs, including those who didn't make it out of Qualifiers or LCK`
+                                   ,rows = `A. All chefs, including those who didn't make it out of Qualifiers or LCK` >=2)
+              ,style = cell_text("white")) %>%
+  # Category B
+  tab_style(locations=cells_body(columns=`B. Chefs with an official placement in their season`
+                                 ,rows = `B. Chefs with an official placement in their season` <.7)
+            ,style = cell_fill("#c85200")) %>%
+  tab_style(locations=cells_body(columns=`B. Chefs with an official placement in their season`
+                                 ,rows = `B. Chefs with an official placement in their season` <.7)
+            ,style = cell_text("white")) %>%
+  tab_style(locations=cells_body(columns=`B. Chefs with an official placement in their season`
+                                 ,rows = `B. Chefs with an official placement in their season` >=.7 &
+                                   `B. Chefs with an official placement in their season` < 1)
+            ,style = cell_fill("#ffbc69")) %>%
+  tab_style(locations=cells_body(columns=`B. Chefs with an official placement in their season`
+                                 ,rows = `B. Chefs with an official placement in their season` == 1)
+            ,style = cell_fill("gray90")) %>%
+  tab_style(locations=cells_body(columns=`B. Chefs with an official placement in their season`
+                                 ,rows = `B. Chefs with an official placement in their season` > 1 &
+                                   `B. Chefs with an official placement in their season` < 2)
+            ,style = cell_fill("#a3cce9")) %>%
+  tab_style(locations=cells_body(columns=`B. Chefs with an official placement in their season`
+                                 ,rows = `B. Chefs with an official placement in their season` >=2)
+            ,style = cell_fill("#1170AA")) %>%
+  tab_style(locations=cells_body(columns=`B. Chefs with an official placement in their season`
+                                 ,rows = `B. Chefs with an official placement in their season` >=2)
+            ,style = cell_text("white")) %>%
+  # Category C
+  tab_style(locations=cells_body(columns=`C. Final 8 chefs`
+                                 ,rows = `C. Final 8 chefs` <.7)
+            ,style = cell_fill("#c85200")) %>%
+  tab_style(locations=cells_body(columns=`C. Final 8 chefs`
+                                 ,rows = `C. Final 8 chefs` <.7)
+            ,style = cell_text("white")) %>%
+  tab_style(locations=cells_body(columns=`C. Final 8 chefs`
+                                 ,rows = `C. Final 8 chefs` >=.7 &
+                                          `C. Final 8 chefs` < 1)
+            ,style = cell_fill("#ffbc69")) %>%
+  tab_style(locations=cells_body(columns=`C. Final 8 chefs`
+                                 ,rows = `C. Final 8 chefs` == 1)
+            ,style = cell_fill("gray90")) %>%
+  tab_style(locations=cells_body(columns=`C. Final 8 chefs`
+                                 ,rows = `C. Final 8 chefs` > 1 &
+                                         `C. Final 8 chefs` < 2)
+            ,style = cell_fill("#a3cce9")) %>%
+  tab_style(locations=cells_body(columns=`C. Final 8 chefs`
+                                 ,rows = `C. Final 8 chefs` >=2)
+            ,style = cell_fill("#1170AA")) %>%
+  tab_style(locations=cells_body(columns=`C. Final 8 chefs`
+                                 ,rows = `C. Final 8 chefs` >=2)
+            ,style = cell_text("white")) %>%
+  # Category D
+  tab_style(locations=cells_body(columns=`D. Final 4 chefs`
+                                 ,rows = `D. Final 4 chefs` <.7)
+            ,style = cell_fill("#c85200")) %>%
+  tab_style(locations=cells_body(columns=`D. Final 4 chefs`
+                                 ,rows = `D. Final 4 chefs` <.7)
+            ,style = cell_text("white")) %>%
+  tab_style(locations=cells_body(columns=`D. Final 4 chefs`
+                                 ,rows = `D. Final 4 chefs` >=.7 &
+                                   `D. Final 4 chefs` < 1)
+            ,style = cell_fill("#ffbc69")) %>%
+  tab_style(locations=cells_body(columns=`D. Final 4 chefs`
+                                 ,rows = `D. Final 4 chefs` == 1)
+            ,style = cell_fill("gray90")) %>%
+  tab_style(locations=cells_body(columns=`D. Final 4 chefs`
+                                 ,rows = `D. Final 4 chefs` > 1 &
+                                   `D. Final 4 chefs` < 2)
+            ,style = cell_fill("#a3cce9")) %>%
+  tab_style(locations=cells_body(columns=`D. Final 4 chefs`
+                                 ,rows = `D. Final 4 chefs` >=2)
+            ,style = cell_fill("#1170AA")) %>%
+  tab_style(locations=cells_body(columns=`D. Final 4 chefs`
+                                 ,rows = `D. Final 4 chefs` >=2)
+            ,style = cell_text("white")) %>%
+  tab_options(
+    row_group.background.color = "gray95",
+    table.font.color = "#323232",
+    table_body.hlines.color = "#323232",
+    table_body.border.top.color = "#323232",
+    heading.border.bottom.color = "#323232",
+    row_group.border.top.color = "#323232",
+    column_labels.border.bottom.color = "#323232",
+    row_group.border.bottom.color = "transparent"
+    ,table.border.top.style = "transparent"
+    ,table.border.bottom.style = "transparent"
+  ) %>%
+  opt_all_caps() %>%
+  cols_width(season ~ px(165)
+             ,`season #` ~ px(85)
+             ,`A. All chefs, including those who didn't make it out of Qualifiers or LCK` ~ px(180)
+             ,`C. Final 8 chefs` ~ px(90)
+             ,`D. Final 4 chefs` ~ px(90)
+             , everything() ~ px(125) )  %>%
+  tab_header(
+    title = "Table 1: Ratio of people of color to white people at different times in Top Chef seasons"
+    ,subtitle = "Ratios above 1 indicate there were more people of color than white people. Ratios under 1 indicate there were fewer people of color than white people. Category A will be the same as Category B for seasons that didn't have Qualifiers or didn't have people competing in Last Chance Kitchen (LCK) who didn't make it into the main competition."
+  )
+
+
+gtsave(REratiotable
+       ,filename = paste(directory,"RE ratio at different times.png"
+                         ,sep=""))
+
+
+## Gender ratio at final 4 compared to winners' gender
+final4 <- alldata %>%
+  filter(timing == "D. Final 4 chefs") %>%
+  select(season,seasonNumber,ratio) %>%
+  left_join(chefdetails %>%
+              filter(placement == "1.0") %>%
+              select(season,seasonNumber,personOfColor) %>%
+              rename(WinnerRE=personOfColor)
+            ) %>%
+  # drop season 22 since they don't have a winner yet
+  filter(!(is.na(WinnerRE)))
+
+final4summary <- final4 %>%
+  group_by(ratio,WinnerRE) %>%
+  summarise(n=n()) %>%
+  ungroup() %>% group_by(ratio) %>%
+  mutate(total = sum(n)
+         ,percent = n/total
+         ,ratio = as.character(ratio)) %>%
+  select(!n) %>%
+  pivot_wider(values_from=percent,names_from=WinnerRE) %>%
+  mutate(white = ifelse(is.na(white),0,white)) %>%
+  pivot_longer(!c(ratio,total),names_to="Winner's race"
+               ,values_to = "percent") %>%
+  mutate(ratio = paste0("Ratio of ",ratio,"\n(",total," seasons)",sep=""))
+
+
+final4pie <- final4summary %>%
+  ggplot(aes(x=1,y=percent,fill=`Winner's race`)) +
+  geom_bar(stat="identity",width=2) +
+  coord_polar(theta="y") +
+  facet_wrap(~ratio) +
+  scale_fill_manual(values=c("#1170AA","#ffbc69"))+
+  ggtitle("Figure 2. Ratio of people of color to white people at the final four of Top Chef seasons and the gender\nof winners in those seasons"
+          ,subtitle = "Ratios above 1 indicate there were more people of color than white people. Ratios under 1 indicate there were fewer people of color than white people.") +
+  labs(caption="Created by Carly Levitz for Pack Your Knives")+
+  theme(
+    axis.ticks = element_blank()
+    ,axis.title = element_blank()
+    ,axis.text = element_blank()
+    ,panel.grid = element_blank()
+  )
+
+ggsave(paste0(directory
+              ,"Gender ratio at F4 and winner gender.png")
+       ,final4pie,width = 8,height = 4,dpi = 1200 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
