6d63080b13920bfabfd453e2cfe262e02371d635
Carly <celevitz@gmail.com>
2025-07-26 11:16:15 -0700

    Winners' confessionals in first episode

--- dev/Confessionals_ComparingWinnersInFirstEpisode.R
+++ dev/Confessionals_ComparingWinnersInFirstEpisode.R
@@ -28,56 +28,121 @@ winners <- confs %>% ungroup() %>%
   ungroup() %>% group_by(series,season,seasonNumber,chef) %>%
   mutate(keep = ifelse(min(episode)==episode,"Keep","Drop")) %>%
   filter((placement == 1 ) &  keep == "Keep") %>% ungroup() %>%
-  select(season,seasonNumber,name,episode,placement,count
-         ,percentdifffromequalinEp,shownthemost,ElimWinner,atJTElim
-         ,OutWithdrewElim) %>%
-  arrange(seasonNumber)
+  select(season,seasonNumber,name,episode,placement,count,equalInEpPercent
+         ,percentdifffromequalinEp,percentofEpsConfs,shownthemost
+         ,ElimWinner,atJTElim,OutWithdrewElim,first) %>%
+  arrange(seasonNumber) %>%
+  # category of winner
+  mutate(category = case_when(ElimWinner == 1 ~ "Won elimination"
+                              ,ElimWinner == 0 & atJTElim == 1 ~ "In the top of elimination"
+                              ,TRUE ~ "Safe")
+         ,xValue=ifelse(placement == 1, 0, NA)
+         ,y = dense_rank(desc(seasonNumber))
+         ,edit = case_when(percentdifffromequalinEp < 0 ~ "Undershown"
+                        ,percentdifffromequalinEp == 0 ~ "Shown as expected"
+                        ,percentdifffromequalinEp > 0 ~ "Overshown")
+         ,first = ifelse(first == "first","*",""))
 
+winnerssimplified <- winners %>%
+  select(season,seasonNumber,name,equalInEpPercent,percentofEpsConfs
+         ,category,xValue,y,edit,first) %>%
+  pivot_longer(!c(season,seasonNumber,name,category,xValue,y,edit,first)
+               ,names_to="variable",values_to="value") %>%
+  # replace category for the percent equal data
+  mutate(category = ifelse(variable %in% "equalInEpPercent"
+                           ,"Expected %",category))
+
+## Winners
+winners %>%
+  ggplot(aes(x=equalInEpPercent,y=y)) +
+  scale_y_continuous(breaks=seq(1,length(unique(winners$seasonNumber)),1)
+                    ,labels=gsub("NA","",paste0("S ",rev(unique(winners$seasonNumber))
+                                   ," winner",winners$first))) +
+  scale_x_continuous(lim=c(0,.12),breaks = seq(0,.12,.01)
+                     ,labels = paste0(seq(0,.12,.01)*100,"%")
+                     ,"% of episode 1's confessionals") +
+  geom_segment(aes(x=equalInEpPercent,xend=percentofEpsConfs
+                ,y=y,yend=y,lty=edit,color=category)) +
+  geom_point(aes(x=percentofEpsConfs,y=y,color=category,fill=category
+                 ,shape=category),cex=5) +
+  geom_point(aes(x=equalInEpPercent,y=y),shape="|",color="black",cex=5) +
+  labs(title="% of confessionals each winner had in the first episode of their season"
+       ,subtitle="Each chef's confessional total is compared to the number if the episode's confessionals were evenly distributed.\n| = expected % of confessionals a chef would receive if everyone received the same amount in the first episode.\n* = chef had the first confessional of the first episode."
+       ,caption = "Created by Carly Levitz for Pack Your Knives") +
+  scale_linetype_manual(values=c("solid","73", "dashed"))+
+  scale_color_manual(values = c("#f8931f","#364156","#57b8ff")) +
+
+  theme_minimal()+
+  theme(
+    panel.grid = element_blank()
+    ,axis.ticks.y = element_blank()
+    ,axis.title.y = element_blank()
+    ,axis.text = element_text(color="black",size = 16)
+    ,axis.title.x = element_text(color="black",size = 20,face="bold")
+    ,axis.line = element_line(color="black")
+    ,axis.ticks.x = element_line(color="black")
+    ,plot.title = element_text(color="black",size = 30,face="bold")
+    ,plot.subtitle = element_text(color="black",size=20)
+    ,plot.caption = element_text(color="black",size=20,hjust=.5)
+    ,panel.background = element_rect(color="white", fill="white")
+    ,plot.background  = element_rect(color="white", fill="white")
+    ,plot.margin = margin(t=25,r=10,l=10,b=10)
+    ,legend.key.size = unit(1, 'cm')
+    ,legend.key.height = unit(1, 'cm')
+    ,legend.key.width = unit(1, 'cm')
+    ,legend.title = element_text(size=18)
+    ,legend.text = element_text(size=16)
+  )
+
+dev.print(png, file = paste0(directory,"WinnersConfsEp1.png")
+          , width = 1200, height = 900)
+dev.off()
+#
 ## Keep just first episode and whoever was shown the most
-shownmost <- confs %>% ungroup() %>%
-  left_join(placement %>%
-              select(season,seasonNumber,chef,placement,name) %>%
-              mutate(placement=as.numeric(placement))) %>%
-  # For each episode, which chef was shown the most?
-  group_by(series,season,seasonNumber,episode) %>%
-  mutate(shownthemost = ifelse(
-    max(percentdifffromequalinEp)==percentdifffromequalinEp
-    ,"Shown the most","Not shown the most")) %>%
-  # first episode that a chef was in
-  ungroup() %>% group_by(series,season,seasonNumber,chef) %>%
-  mutate(keep = ifelse(min(episode)==episode,"Keep","Drop")) %>%
-  filter(shownthemost == "Shown the most" &  keep == "Keep") %>% ungroup() %>%
-  select(season,seasonNumber,name,episode,placement,count
-         ,percentdifffromequalinEp,shownthemost,ElimWinner,atJTElim
-         ,OutWithdrewElim) %>%
-  arrange(seasonNumber)
-
-## Combine
-combined <- winners %>%
-  bind_rows(shownmost) %>%
-  mutate(y = dense_rank(desc(seasonNumber))
-         ,`Elimination challenge outcome` = case_when(ElimWinner == 1 ~ "Won"
-                            ,OutWithdrewElim == 1 ~ "Eliminated"
-                            ,atJTElim == 1 ~ "At judges' table"
-                            ,TRUE ~ "Safe")
-         ,`Category of chef` = ifelse(placement == 1,"Season winner"
-                                ,"Most confessionals in 1st episode")
-         ,xValue=ifelse(placement == 1, 0, NA))
-numberofseasons <- length(unique(combined$seasonNumber))
-
-combined %>%
-  ggplot(aes(x= percentdifffromequalinEp,y=y
-             ,shape=`Elimination challenge outcome`
-             ,color=`Category of chef`,fill=`Category of chef`))+
-  geom_rect(aes(xmin=0,xmax=0,ymin=1,ymax=numberofseasons),color="gray47") +
-  geom_point() +
-  scale_y_continuous(breaks=seq(1,length(unique(combined$seasonNumber)),1)
-                     ,labels=paste0("S ",rev(unique(combined$seasonNumber)))) +
-  scale_x_continuous(lim=c(-.5,1.6),breaks = seq(-.5,1.5,.5)
-                     ,labels = seq(-.5,1.5,.5)) +
-  theme_minimal() +
-  geom_rect(aes(xmin=percentdifffromequalinEp,xmax=xValue,ymin=y,ymax=y))
-
-
-
+# shownmost <- confs %>% ungroup() %>%
+#   left_join(placement %>%
+#               select(season,seasonNumber,chef,placement,name) %>%
+#               mutate(placement=as.numeric(placement))) %>%
+#   # For each episode, which chef was shown the most?
+#   group_by(series,season,seasonNumber,episode) %>%
+#   mutate(shownthemost = ifelse(
+#     max(percentdifffromequalinEp)==percentdifffromequalinEp
+#     ,"Shown the most","Not shown the most")) %>%
+#   # first episode that a chef was in
+#   ungroup() %>% group_by(series,season,seasonNumber,chef) %>%
+#   mutate(keep = ifelse(min(episode)==episode,"Keep","Drop")) %>%
+#   filter(shownthemost == "Shown the most" &  keep == "Keep") %>% ungroup() %>%
+#   select(season,seasonNumber,name,episode,placement,count,equalInEpPercent
+#          ,percentdifffromequalinEp,shownthemost,ElimWinner,atJTElim
+#          ,OutWithdrewElim) %>%
+#   arrange(seasonNumber)
+#
+# ## Combine
+# combined <- winners %>%
+#   bind_rows(shownmost) %>%
+#   mutate(y = dense_rank(desc(seasonNumber))
+#          ,`Elimination challenge outcome` = case_when(ElimWinner == 1 ~ "Won"
+#                             ,OutWithdrewElim == 1 ~ "Eliminated"
+#                             ,atJTElim == 1 ~ "At judges' table"
+#                             ,TRUE ~ "Safe")
+#          ,`Category of chef` = ifelse(placement == 1,"Season winner"
+#                                 ,"Most confessionals in 1st episode")
+#          ,xValue=ifelse(placement == 1, 0, NA))
+# numberofseasons <- length(unique(combined$seasonNumber))
+#
+# combined %>%
+#   ggplot(aes(x= percentdifffromequalinEp,y=y
+#              ,shape=`Elimination challenge outcome`
+#              ,color=`Category of chef`,fill=`Category of chef`))+
+#   geom_rect(aes(xmin=0,xmax=0,ymin=1,ymax=numberofseasons),color="gray47") +
+#   geom_point() +
+#   scale_y_continuous(breaks=seq(1,length(unique(combined$seasonNumber)),1)
+#                      ,labels=paste0("S ",rev(unique(combined$seasonNumber)))) +
+#   scale_x_continuous(lim=c(-.5,1.6),breaks = seq(-.5,1.5,.5)
+#                      ,labels = seq(-.5,1.5,.5)) +
+#   theme_minimal() +
+#   geom_rect(aes(xmin=percentdifffromequalinEp,xmax=xValue,ymin=y,ymax=y))
+#
+#
+#
 
