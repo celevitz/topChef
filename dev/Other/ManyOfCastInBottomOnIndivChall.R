867f00f349e7e6fea08e36a41e423149e435d3f7
Carly <celevitz@gmail.com>
2026-03-25 13:26:33 -0700

    Add in team/indiv clarification

--- dev/Other/ManyOfCastInBottomOnIndivChall.R
+++ dev/Other/ManyOfCastInBottomOnIndivChall.R
@@ -5,13 +5,18 @@ directory <- "/Users/carlylevitz/Documents/Data/topChef/"
 
 # Bring in data
 challenges <- read.csv(paste0(directory,"Top Chef - Challenge wins.csv"))
+descr <- read.csv(paste0(directory,"Top Chef - Challenge descriptions.csv"))
 
 # For each challenge, how many people are in the competition, and how many
 # are in a given outcome?
 data <- challenges %>%
+  # add on info about if it was team or indiv
+  left_join(descr %>% select(season,seasonNumber,series,episode,challengeType
+                             ,outcomeType)) %>%
   # remove those not in the competition
-  filter(inCompetition == TRUE) %>%
-  group_by(series,season,seasonNumber,episode,challengeType) %>%
+  # and keep just US series
+  filter(inCompetition == TRUE & series == "US" ) %>%
+  group_by(series,season,seasonNumber,episode,challengeType,outcomeType) %>%
   summarise(numberofchefscompeting = n()
          # how many of those chefs are in the bottom, per challenge?
          ,bottom = sum(ifelse(outcome %in% c("LOW","OUT","RUNNER-UP","WITHDREW"
@@ -21,5 +26,6 @@ data <- challenges %>%
 filtered <- data %>%
   # look at the ones where at least half are in the bottom
   # and just US series - when there are at least 9 chefs in comp
-  filter(percent > .5 & series == "US" & numberofchefscompeting >=9)
+  filter(percent > .5 & numberofchefscompeting >=9)
 
+data %>% filter(percent > .7)
