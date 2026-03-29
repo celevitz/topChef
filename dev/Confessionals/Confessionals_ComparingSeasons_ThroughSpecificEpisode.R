850f3f4da500835132eb344ca7287db148b92652
Carly <celevitz@gmail.com>
2026-03-29 09:52:40 -0700

    Add visual

--- dev/Confessionals/Confessionals_ComparingSeasons_ThroughSpecificEpisode.R
+++ dev/Confessionals/Confessionals_ComparingSeasons_ThroughSpecificEpisode.R
@@ -78,6 +78,41 @@ eliminated <- c("Jaspratap Bindra","Day Anaїs Joseph","Nana Araba Wilmot")
     select(chef,difffromexpected) %>%
     print(n=30)
 
+# Let's see what sort of visual I can do
+  visualdata <- confs %>%
+    mutate(chef = str_split_i(chef," ",1)
+           ,chef = fct_reorder(paste0(chef," S",seasonNumber), difffromexpected)
+           ,difffromexpected=100*difffromexpected
+           ,Legend = case_when(seasonNumber == 23 ~ "S23"
+                              ,placement == 1 ~ "Winner"
+                              ,placement %in% c(2,3) ~ "2nd or 3rd"
+                              ,placement %in% c(4,5,6) ~ "4th thru 6th" ))
+
+  visualdata %>%
+    ggplot(aes(x=difffromexpected,y=chef,color=Legend,fill=Legend,shape=Legend)) +
+    geom_point(size=7) +
+    ggtitle("% difference from expected # of confessionals thru Ep. 3"
+            ,subtitle="Expected is when all chefs have the same number of confessionals") +
+    xlab("Percent difference from expected") +
+    ylab("") +
+    theme_minimal() +
+    theme(axis.text.y = element_text(color="black",size = 20)
+          ,axis.text.x = element_text(color="black",size = 16)
+          ,axis.title = element_text(color="black",size = 20,face="bold")
+          ,plot.title.position = "plot" # put the title across whole plot
+          ,plot.title = element_text(color="black",size = 30,face="bold")
+          ,plot.subtitle = element_text(color="black",size=20)
+          ,plot.caption = element_text(color="black",size=20,hjust=.5)
+          ,legend.key.size = unit(1, 'cm') #change legend key size
+          ,legend.key.height = unit(1, 'cm') #change legend key height
+          ,legend.key.width = unit(1, 'cm') #change legend key width
+          ,legend.title = element_text(size=20) #change legend title font size
+          ,legend.text = element_text(size=16)) #change legend text font size
+
+  dev.print(png, file = paste0(directory,"Confessionals_ComparingSeasonsThroughASpecificEpisodeNumber.png")
+            , height = 1600, width = 900)
+  dev.off()
+
 
 
 
