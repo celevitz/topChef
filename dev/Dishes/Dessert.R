a65b2735972321dc603879124afd17c2230dbd45
Carly <celevitz@gmail.com>
2026-04-20 17:34:08 -0700

    Outcome analysis of required dessert dishes vs non-required

--- dev/Dishes/Dessert.R
+++ dev/Dishes/Dessert.R
@@ -12,4 +12,14 @@ desserts <- dishesraw %>%
   filter(dessert == 1) %>%
   select(series,season,seasonNumber,episode,chef,challengeType
          ,dish,outcome,outcomeType,notes) %>%
-  mutate(required = ifelse(grepl("required",notes),1,0))
+  mutate(required = ifelse(grepl("required",notes),1,0)
+         ,outcomesimplified = case_when(outcome == "RUNNER-UP"~"OUT"
+                                       ,outcome == "WINNER"~"WIN"
+                                       ,TRUE ~ outcome)
+         ,outcomesimplified2 = case_when(outcomesimplified %in%
+                                                          c("HIGH","LOW")~"IN"
+                                         ,TRUE ~ outcomesimplified))
+
+# How well do people do depending on if the challenge required a dessert?
+table(desserts$outcomesimplified2,desserts$required)
+chisq.test(desserts$outcomesimplified2,desserts$required)
